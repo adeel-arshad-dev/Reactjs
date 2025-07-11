462b3197ffc6093c6b3f96449f691962af937e66
Adeel Arshad <adeelarshad.developer@gmail.com>
2025-07-11 11:24:50 +0000

    Setting up react using vite

---
+++ reactSetup/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>We have removed some files </h1>
+    </>
+  )
+}
+
+export default App
