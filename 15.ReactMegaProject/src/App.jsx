291b6a392f17ee4e7cc818768be68fb4e3d97dd8
Adeel Arshad <adeelarshad.developer@gmail.com>
2025-07-25 07:24:14 +0000

    Blogging App

--- 15.ReactMegaProject/src/App.jsx
+++ 15.ReactMegaProject/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 
 function App() {
 
-
+console.log(import.meta.env.VITE_APPWRITE_URL)
+console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID)
   return (
     <>
      <h1>App write</h1>
