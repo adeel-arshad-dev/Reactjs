ed112ea76d340e971832f8eb2342aeb04d1de80b
Adeel Arshad <adeelarshad.developer@gmail.com>
2025-07-26 11:40:20 +0000

    Creating And Updating posts and files

--- 15.ReactMegaProject/src/appwrite/config.js
+++ 15.ReactMegaProject/src/appwrite/config.js
@@ -33,7 +33,98 @@ async createPost({title,slug, content, featuredImage, status,userId}) {
     }
 }
 
+async updatePost(slug,{title, content, featuredImage, status}) {
+    try {
+        return await this.databases.updateDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status
+            }
+        );
+    } catch (error) {
+        throw new Error("Error updating post: " + error.message);
+    }
+
 }
 
+async deletePost(slug){
+   try {
+    return await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug)
+        return true;
+    
+   } catch (error) {
+       throw new Error("Error deleting post: " + error.message);
+    
+   }
+   return false;
+}
+
+async getPost() {
+    try{
+ return this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+       slug
+ )
+    }
+    catch{
+        throw new Error("Error fetching posts");
+      }
+    }
+
+async getPosts(quaries=[Query.equal("status","active")]){
+    try {
+        return await this.databases.listDocuments(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            quaries
+        );
+    } catch (error) {
+        throw new Error("Error fetching posts: " + error.message);
+    }
+    return false;
+}
+// File upload and management methods
+async uploadFile(file){
+try {
+    return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+    )
+    
+} catch (error) {
+    throw new Error("Error uploading file: " + error.message);
+}
+return false;
+}
+
+async deleteFile(fileId){
+  try {
+    return this.bucket.deleteFile(
+        conf.appwriteBucketId,
+        fileId
+    )
+  } catch (error) {
+    throw new Error("Error deleting file: " + error.message);
+  }
+  return false;
+}
+
+getFilePreview(fileId) {
+    return this.bucket.getFilePreview(
+        conf.appwriteBucketId,
+        fileId
+    );
+}
+}
 const service=new Service();
 export default service
